40610e21fc12664a8e513e0818bc1f2469afa044
Jesus Diaz <jesusdiaz1403@gmail.com>
2024-11-17 17:50:10 -0500

    Added Retrival Function for the User

--- Database Connection.py
+++ Database Connection.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 client = MongoClient("mongodb+srv://jesusdiaz1403:b1mRAtZw3MK5Q5eO@moneymap.mpksh.mongodb.net/")
 
 db = client["MoneyMap"]
-guestInfo = db["Guest"]
+
 userInfo = db["User"]
 
 def userData():
@@ -15,17 +15,24 @@ def userData():
     user_info["CreditCard"] = float(input("Enter your credit card balance: "))
     user_info["Loans"] = float(input("Enter your loan balance: "))
 
-def guestData():
-    guest_info = {}
-    guest_info["MonthlyIncome"] = float(input("Enter your monthly income: "))
-    guest_info["MonthlyExpenses"] = float(input("Enter your monthly expenses: "))
-
 def saveUser(userData):
     userInfo.insert_one(userData)
     print("User Info Updated!")
 
-def saveGuest(GuestData):
-    guestInfo.insert_one(GuestData)
-    print("Guest Info Updated!")
+def get_user(user_id):
+    user = userInfo.find_one({"_id": user_id}, ["MonthlyIncome", "MonthlyExpenses", "Checking", "Savings", "CreditCard", "Loans"])
+    if user:
+        return {
+            "MonthlyIncome": user["MonthlyIncome"],
+            "MonthlyExpenses": user["MonthlyExpenses"],
+            "Checking": user["Checking"],
+            "Savings": user["Savings"],
+            "CreditCard": user["CreditCard"],
+            "Loans": user["Loans"]
+        }
+    else:
+        print("User not found")
+        return None
+
     
 
